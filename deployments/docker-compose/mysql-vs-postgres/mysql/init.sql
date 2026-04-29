5b720142624e03bcf52fcaccfdf66f42aa8cf14d
Gushchin Andrei <neogan74@gmail.com>
2026-04-29 13:09:48 +0200

    fix: remove unsupported IF NOT EXISTS from MySQL CREATE INDEX statements

--- deployments/docker-compose/mysql-vs-postgres/mysql/init.sql
+++ deployments/docker-compose/mysql-vs-postgres/mysql/init.sql
@@ -10,9 +10,9 @@ CREATE TABLE IF NOT EXISTS orders (
 
 -- Functional index on user.country: used by query + update benchmarks
 -- Requires MySQL 8.0.13+
-CREATE INDEX IF NOT EXISTS idx_orders_country
+CREATE INDEX idx_orders_country
     ON orders ((doc->>'$.user.country'));
 
 -- Functional index on status
-CREATE INDEX IF NOT EXISTS idx_orders_status
+CREATE INDEX idx_orders_status
     ON orders ((doc->>'$.status'));
